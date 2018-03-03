b56e268815fe2c4c20252078677db7debf47c1de
Don Maxito <maxharz@gmail.com>
2018-03-02 23:00:24 -0600

    adapt new boruca style

--- network.js
+++ network.js
@@ -1,23 +1,15 @@
-import Boruca from '/libraries/boruca-messaging/src/boruca.js';
+import { EventServer, RPC } from '/libraries/boruca-messaging/src/boruca.js';
 import NanoNetworkApi from '/libraries/nano-api/nano-network-api.js';
-import config from './config.js';
 
 class Network {
-
     constructor() {
-        // Check if we run in iframe or were opened by window.open()
-        this._communicationTarget = window.parent || window.opener;
-
-        const that = this;
-
-        if (this._communicationTarget) {
-            this._connect();
-        }
+        this.connect();
     }
 
-    async _connect() {
-        const { proxy, stub } = await Boruca.proxy(this._communicationTarget, this._communicationTarget.origin, NanoNetworkApi);
-        stub.fire = (event, value) => proxy.fire(event, value);
+    async connect() {
+        const eventServer = new EventServer();
+        const stub = RPC.Server(NanoNetworkApi);
+        stub.fire = (event, value) => eventServer.fire(event, value);
         await stub.init();
         await stub.connect();
     }
