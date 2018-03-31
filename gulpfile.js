d57af8b694c1b4c325ad3976fdd860334864367c
Don Maxito <maxharz@gmail.com>
2018-03-31 11:42:32 -0600

    yargs

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const NimiqBuild = require('../../meta/build-process/nimiq-base-gulpfile.js');
 gulp.task('build', () => NimiqBuild.build(
     'src/network.js',
     'src/dummy.css',
-    'src/index.html',
+    'src/network.html',
     [],
     `${__dirname}/../../`,
     'dist',
