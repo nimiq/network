3901fab570cea043f090e16e5f0ba2e7099b4f92
Don Maxito <maxharz@gmail.com>
2018-04-01 22:24:10 -0600

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 const gulp = require('gulp');
 
-const argv = require('yargs').argv;
-
 const NimiqBuild = require('../../meta/build-process/nimiq-base-gulpfile.js');
 
-gulp.task('build', () => NimiqBuild.build(
-    'src/network.js',
-    'src/dummy.css',
-    'src/index.html',
-    [],
-    `${__dirname}/../../`,
-    'dist',
-    argv.config,
-    true
-));
+gulp.task('build', () => NimiqBuild.build({
+    jsEntry: 'src/network.js',
+    htmlEntry: 'src/index.html',
+    rootPath: `${__dirname}/../../`,
+    distPath: 'dist',
+    minify: false
+}));
 
 gulp.task('default', ['build']);
