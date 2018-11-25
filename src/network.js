dd3d0f98aea4d7cfbe3d6a8d1c0ced1d309d509c
sisou <hello@soerenschwert.de>
2018-11-25 08:44:59 -0600

    Remove unnecessary double brackets

--- src/network.js
+++ src/network.js
@@ -10,17 +10,17 @@ export class Network extends NanoNetworkApi {
         this._eventServer = new EventServer();
 
         // Register RPC calls.
-        this._eventServer.onRequest('relayTransaction', ((state, arg) => this.relayTransaction(arg)));
-        this._eventServer.onRequest('getTransactionSize', ((state, arg) => this.getTransactionSize(arg)));
-        this._eventServer.onRequest('subscribe', ((state, arg) => this.subscribe(arg)));
-        this._eventServer.onRequest('getBalance', ((state, arg) => this.getBalance(arg)));
-        this._eventServer.onRequest('getAccountTypeString', ((state, arg) => this.getAccountTypeString(arg)));
+        this._eventServer.onRequest('relayTransaction', (state, arg) => this.relayTransaction(arg));
+        this._eventServer.onRequest('getTransactionSize', (state, arg) => this.getTransactionSize(arg));
+        this._eventServer.onRequest('subscribe', (state, arg) => this.subscribe(arg));
+        this._eventServer.onRequest('getBalance', (state, arg) => this.getBalance(arg));
+        this._eventServer.onRequest('getAccountTypeString', (state, arg) => this.getAccountTypeString(arg));
         this._eventServer.onRequest(
             'requestTransactionHistory',
-            ((state, addresses, knownReceipts, fromHeight) => this.requestTransactionHistory(addresses, knownReceipts, fromHeight)),
+            (state, addresses, knownReceipts, fromHeight) => this.requestTransactionHistory(addresses, knownReceipts, fromHeight),
         );
-        this._eventServer.onRequest('getGenesisVestingContracts', ((state, arg) => this.getGenesisVestingContracts(arg)));
-        this._eventServer.onRequest('removeTxFromMempool', ((state, arg) => this.removeTxFromMempool(arg)));
+        this._eventServer.onRequest('getGenesisVestingContracts', (state, arg) => this.getGenesisVestingContracts(arg));
+        this._eventServer.onRequest('removeTxFromMempool', (state, arg) => this.removeTxFromMempool(arg));
     }
 
     fire(event, data) {
