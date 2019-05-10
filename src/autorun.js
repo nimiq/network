1d25d2d94fb10f262d922e2bc1508ed51f392e35
Don Maxito <maxharz@gmail.com>
2019-05-10 14:28:32 +0200

    make config explicit; update dependencies

--- src/autorun.js
+++ src/autorun.js
@@ -1,5 +1,21 @@
 import { Network } from './network';
-import { Config } from '@nimiq/utils';
 
-const network = new Network(Config);
+const CORE_VERSION = '1.4.1';
+
+function getConfig() {
+    switch (location.origin) {
+        case 'nimiq.com':
+            return {
+                cdn: `https://cdn.nimiq.com/v${CORE_VERSION}/nimiq.js`,
+                network: 'main',
+            };
+        default:
+            return {
+                cdn: `https://cdn.nimiq-testnet.com/v${CORE_VERSION}/nimiq.js`,
+                network: 'test',
+            }
+    }
+}
+
+const network = new Network(getConfig());
 network.connectPico([], false).catch(console.error);
