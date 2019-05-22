54ace70c34e2ed46d16ea8a7caf6d5e8dc16343f
sisou <hello@soerenschwert.de>
2019-05-23 01:05:00 +0200

    Fix test for configuration

--- src/autorun.js
+++ src/autorun.js
@@ -3,8 +3,8 @@ import { Network } from './network';
 const CORE_VERSION = '1.4.1';
 
 function getConfig() {
-    switch (location.origin) {
-        case 'nimiq.com':
+    switch (location.host) {
+        case 'network.nimiq.com':
             return {
                 cdn: `https://cdn.nimiq.com/v${CORE_VERSION}/nimiq.js`,
                 network: 'main',
