4056dd970e50bf24f6dea29c23e207e63d9ba3bc
sisou <hello@soerenschwert.de>
2020-07-22 20:39:25 +0200

    Use CDN version 1.5.7 in testnet

--- src/v2/autorun.js
+++ src/v2/autorun.js
@@ -1,6 +1,6 @@
 import { Network } from './network';
 
-const CORE_VERSION = '1.5.6';
+const CORE_VERSION = '1.5.7';
 
 function getConfig() {
     switch (location.host) {
@@ -12,8 +12,7 @@ function getConfig() {
             };
         default:
             return {
-                // cdn: `https://cdn.nimiq-testnet.com/v${CORE_VERSION}/nimiq.js`,
-                cdn: `/nimiq/nimiq.js`,
+                cdn: `https://cdn.nimiq-testnet.com/v${CORE_VERSION}/nimiq.js`,
                 network: 'test',
             }
     }
