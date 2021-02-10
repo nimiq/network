63a3050cc9c83e454aed4b74ed6e28f2aef61d6f
Sören <hello@soerenschwert.de>
2021-02-10 11:50:40 -0600

    Use custom Nimiq lib in testnet, including consensus deadlock fixes

--- src/v2/autorun.js
+++ src/v2/autorun.js
@@ -11,7 +11,8 @@ function getConfig() {
             };
         default:
             return {
-                cdn: `https://cdn.nimiq-testnet.com/v${CORE_VERSION}/nimiq.js`,
+                // cdn: `https://cdn.nimiq-testnet.com/v${CORE_VERSION}/nimiq.js`,
+                cdn: `/nimiq/nimiq.js`,
                 network: 'test',
             }
     }
