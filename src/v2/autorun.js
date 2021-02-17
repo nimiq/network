a96acee5977bbab8c4e6cc43b27dc634a4885aa8
Sören <hello@soerenschwert.de>
2021-02-17 15:19:01 -0600

    Use v1.5.8 in both testnet and mainnet

--- src/v2/autorun.js
+++ src/v2/autorun.js
@@ -1,6 +1,6 @@
 import { Network } from './network';
 
-const CORE_VERSION = '1.5.7';
+const CORE_VERSION = '1.5.8';
 
 function getConfig() {
     switch (location.host) {
@@ -11,8 +11,7 @@ function getConfig() {
             };
         default:
             return {
-                cdn: `https://cdn.nimiq-testnet.com/v1.5.8/nimiq.js`,
-                // cdn: `/nimiq/nimiq.js`,
+                cdn: `https://cdn.nimiq-testnet.com/v${CORE_VERSION}/nimiq.js`,
                 network: 'test',
             }
     }
