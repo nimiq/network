daa560e58a6cafd89568659bcca305219cebbf42
Daniel <github.danimoh@neverbox.com>
2022-03-13 22:34:38 +0100

    Make sure rpc calls do not return a value are correctly resolved

--- src/v2/network.js
+++ src/v2/network.js
@@ -1,4 +1,5 @@
 import { EventServer } from '@nimiq/rpc-events';
+import { ResponseStatus } from '@nimiq/rpc';
 import { NanoApi } from '@nimiq/nano-api';
 
 export class Network extends NanoApi {
@@ -10,35 +11,51 @@ export class Network extends NanoApi {
         this._eventServer = new EventServer();
 
         // Register RPC calls.
-        this._eventServer.onRequest('connect', () => { this.connect(); });
-        this._eventServer.onRequest('disconnect', (state, reason) => this.disconnect(reason));
-        this._eventServer.onRequest('connectPico', (state, addresses) => this.connectPico(addresses));
-        this._eventServer.onRequest('relayTransaction', (state, arg) => this.relayTransaction(arg));
-        this._eventServer.onRequest('getTransactionSize', (state, arg) => this.getTransactionSize(arg));
-        this._eventServer.onRequest('subscribe', (state, arg) => this.subscribe(arg));
-        this._eventServer.onRequest('getBalance', (state, arg) => this.getBalance(arg));
-        this._eventServer.onRequest('forgetBalances', (state, arg) => this.forgetBalances(arg));
-        this._eventServer.onRequest('getAccounts', (state, arg) => this.getAccounts(arg));
-        this._eventServer.onRequest('getAccountTypeString', (state, arg) => this.getAccountTypeString(arg));
-        this._eventServer.onRequest(
-            'requestTransactionHistory',
-            (state, addresses, knownReceipts, fromHeight) => this.requestTransactionHistory(addresses, knownReceipts, fromHeight),
+        this._registerCommand(
+            'connect',
+            () => this.connect().then((client) => undefined), // result client cannot be passed via postmessage
         );
-        this._eventServer.onRequest('requestTransactionReceipts', (state, address, limit) => this.requestTransactionReceipts(address, limit));
-        this._eventServer.onRequest('getGenesisVestingContracts', (state, modern) => this.getGenesisVestingContracts(modern));
-        this._eventServer.onRequest('removeTxFromMempool', (state, arg) => this.removeTxFromMempool(arg));
-        this._eventServer.onRequest('getPeerAddresses', (state) => this.getPeerAddresses());
-        this._eventServer.onRequest('resetConsensus', (state) => this.client.resetConsensus());
+        this._registerCommand('connectPico', this.connectPico);
+        this._registerCommand('disconnect', this.disconnect);
+        this._registerCommand('relayTransaction', this.relayTransaction);
+        this._registerCommand('getTransactionSize', this.getTransactionSize);
+        this._registerCommand('subscribe', this.subscribe);
+        this._registerCommand('getBalance', this.getBalance);
+        this._registerCommand('forgetBalances', this.forgetBalances);
+        this._registerCommand('getAccounts', this.getAccounts);
+        this._registerCommand('getAccountTypeString', this.getAccountTypeString);
+        this._registerCommand('requestTransactionHistory', this.requestTransactionHistory);
+        this._registerCommand('requestTransactionReceipts', this.requestTransactionReceipts);
+        this._registerCommand('getGenesisVestingContracts', this.getGenesisVestingContracts);
+        this._registerCommand('removeTxFromMempool', this.removeTxFromMempool);
+        this._registerCommand('getPeerAddresses', this.getPeerAddresses);
+        this._registerCommand('resetConsensus', () => this.client.resetConsensus());
 
         // Modern
-        this._eventServer.onRequest('sendTransaction', (state, tx) => this.sendTransaction(tx));
-        this._eventServer.onRequest('getTransactionsByAddress', (state, address, sinceHeight, knownDetails, limit) => this.getTransactionsByAddress(address, sinceHeight, knownDetails, limit));
-        this._eventServer.onRequest('addTransactionListener', (state, eventName, addresses) => this.addTransactionListener(eventName, addresses));
-        this._eventServer.onRequest('addConsensusChangedListener', (state, eventName) => this.addConsensusChangedListener(eventName));
-        this._eventServer.onRequest('removeListener', (state, handle) => this.removeListener(handle));
+        this._registerCommand('sendTransaction', this.sendTransaction);
+        this._registerCommand('getTransactionsByAddress', this.getTransactionsByAddress);
+        this._registerCommand('addTransactionListener', this.addTransactionListener);
+        this._registerCommand('addConsensusChangedListener', this.addConsensusChangedListener);
+        this._registerCommand('removeListener', this.removeListener);
     }
 
     fire(event, data) {
         this._eventServer.fire(event, data);
+    }
+
+    _registerCommand(command, handler) {
+        this._eventServer.onRequest(
+            command,
+            // defining a bunch of parameters instead of only just using ...args as single rest parameter to circumvent
+            // rpc lib's maximum expected arguments count check which is not compatible with rest parameters
+            async (state, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, ...args) => {
+                const result = await (handler.bind(this)(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, ...args));
+                if (result !== undefined) return result;
+                else {
+                    // we have to reply manually to resolve the rpc call
+                    state.reply(ResponseStatus.OK, result);
+                }
+            },
+        );
     }
 }
